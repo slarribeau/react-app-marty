ba033fb154203fa4a7e59fd2b3e122f01cce58f4
slarribeau <slarribeau@gmail.com>
2018-11-19 14:47:03 -0800

    limping, but oh so slowly

---
+++ src/components/Standing.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const Standing = (props) => {
+/*    state = {
+                standings: [],
+                date:null,
+                season:null,
+                league:null,
+                division:null
+            };
+*/
+
+    return  this.props.state.standings.map((standing) => {
+
+        <div className= "ui container" style={{marginTop:'10px'}}>
+            standing
+        </div>
+        }
+    );
+}
+
+export default App;   
