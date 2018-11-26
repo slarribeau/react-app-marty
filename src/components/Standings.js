e9760c7114d6ed7c9fe46a94561ef9f226d76860
slarribeau <slarribeau@gmail.com>
2018-11-26 11:58:45 -0800

    not quite working, the standings format that is, but cheking in because this is a decent place and only the famous last bug exists. haha

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -9,9 +9,9 @@ const Standings = (props) => {
     const result = props.standings.filter(x => x.Date === tmpDate);
     const result2 = result.filter(x => x.League === props.league);
     const result3 = result2.filter(x => x.Division === props.division);
-//                {standing.Team} {standing.W} {standing.L} {standing.GB} {standing.PCT}
 
     const x = result3.map((standing) => {
+        //{standing.Team} {standing.W} {standing.L} {standing.GB} {standing.PCT}
         return (
             <div>
                 <td style={{"width":"50px"}}>{standing.Team}  </td>
@@ -25,19 +25,16 @@ const Standings = (props) => {
     console.log(x)
     return(
         <div>
-            <table style={{"fontFamily":"Courier"}}>
+            <table>
                 <tr> 
-                    <th></th>
-                    <th style={{"fontFamily":"Helvetica"}}>TEAM Larribeau</th>
+                    <th style={{"width":"250px"}}>TEAM</th>
                     <th style={{"textAlign":"right"}}>W</th>
                     <th style={{"textAlign":"right"}}>L</th>
                     <th>PCT</th>
                     <th>GB</th>
                 </tr>
                 <tr>
-                    <td>
                       {x}
-                    </td>
                 </tr>
             </table>
         </div>
@@ -45,28 +42,3 @@ const Standings = (props) => {
 }
 
 export default Standings;   
-/*
-    <table>
-      <tr> 
-        <th></th>
-        <th>Team</th>
-        <th style="text-align:right">W</th>
-        <th style="text-align:right">L</th>
-        <th>PCT</th>
-        <th>GB</th>
-
-      </tr>
-      <!-- <tr *ngFor="let standing of standings | divisionFilter:(division) | filter:(date)">  -->
-      <tr *ngFor="let standing of standings | leagueFilter:(league) | divisionFilter:(division) | filter:(date)">
-
-        <td> 
-          <img src="assets/icon.png" alt="some text" width=16 height=16 style="float:left;">
-        </td>
-        <td style="width:50px;  ">{{standing.Team}}  </td>
-        <td style="width:150px; ">{{standing.W}} </td>
-        <td style="width:100px; ">{{standing.L}}</td>
-        <td style="width:200px; ">{{standing.PCT}}</td>
-        <td style="width:200px; ">{{standing.GB}}</td>
-      </tr>
-    </table>
-*/
