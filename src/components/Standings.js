06f2d51bc79bacf2f58a8f6d9d2795c8f0aa07cd
slarribeau <slarribeau@gmail.com>
2018-11-26 12:33:04 -0800

    Not finished, but site is taking a whole different look with a fancier table css

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -13,13 +13,13 @@ const Standings = (props) => {
     const x = result3.map((standing) => {
         //{standing.Team} {standing.W} {standing.L} {standing.GB} {standing.PCT}
         return (
-            <div>
-                <td style={{"width":"50px"}}>{standing.Team}  </td>
-                <td style={{"width":"150px"}}>{standing.W} </td>
-                <td style={{"width":"100px"}}>{standing.L}</td>
-                <td style={{"width":"200px"}}>{standing.PCT}</td>
-                <td style={{"width":"200px"}}>{standing.GB}</td>
-            </div>
+            <tr>
+                <td style={{}}>{standing.Team}  </td>
+                <td style={{}}>{standing.W} </td>
+                <td style={{}}>{standing.L}</td>
+                <td style={{}}>{standing.PCT}</td>
+                <td style={{}}>{standing.GB}</td>
+            </tr>
         );
     });
     console.log(x)
@@ -27,15 +27,13 @@ const Standings = (props) => {
         <div>
             <table>
                 <tr> 
-                    <th style={{"width":"250px"}}>TEAM</th>
-                    <th style={{"textAlign":"right"}}>W</th>
-                    <th style={{"textAlign":"right"}}>L</th>
+                    <th style={{}}>TEAM</th>
+                    <th style={{}}>W</th>
+                    <th style={{}}>L</th>
                     <th>PCT</th>
                     <th>GB</th>
                 </tr>
-                <tr>
                       {x}
-                </tr>
             </table>
         </div>
     );
