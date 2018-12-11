9ffabfc6f2b7910923d164cc9a0bcf1b049f622f
slarribeau <slarribeau@gmail.com>
2018-12-11 15:38:45 -0800

    figured out how to set pull down colors

--- src/components/Selectbox.js
+++ src/components/Selectbox.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 import Select from 'react-select'
 
+const colourStyles = {
+  option: (provided, state) => ({
+    ...provided,
+    color: state.isSelected ? 'white' : 'grey',
+    backgroundColor: 'black',
+    padding: 10,
+  }),
+  control: base => ({
+    ...base,
+    background: "black"
+  }),
+  singleValue: base => ({
+    ...base,
+    color: "grey"
+  }),
+  input: base => ({
+    ...base,
+    color: 'black'
+  })
+};
+
 class Selectbox extends Component {
 
   onChange = (e) => {
@@ -13,6 +34,7 @@ class Selectbox extends Component {
           onChange={ (e) => this.onChange(e) }
           defaultValue={this.props.options[0]}
           options={this.props.options}
+          styles={colourStyles}
         />   
     );
   }
